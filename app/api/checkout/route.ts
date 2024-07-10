01f1a7a6f5039b991219ca2061aee127f33c8243
Rehan Baig <rehanbaigofficialmail@gmail.com>
2024-07-10 20:20:20 +0530

    Crosheaders fix 2

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: NextRequest) {
             payment_method_types: ["card"],
             mode: "payment",
             shipping_address_collection: {
-                allowed_countries: ["US", "CA"],
+                allowed_countries: ["US"],
             },
             shipping_options: [
                 { shipping_rate: "shr_1PPoRT2K2Y5NYy0GKvwBL349" },
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
             ],
             line_items: cartItems.map((cartItem: any) => ({
                 price_data: {
-                    currency: "cad",
+                    currency: "usd",
                     product_data: {
                         name: cartItem.item.title,
                         metadata: {
